8f4f5af816d48c7e37f073fe8802ba5b96f1072f
Nishant Raj <103452742+Nishant6992@users.noreply.github.com>
2024-10-14 17:36:53 +0530

    Add files via upload

---
+++ EmployeeDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CRUDUsingQueryAJAX.Models
+{
+    public class EmployeeDBContext : DbContext
+    {
+
+       // Constructor calling the Base DbContext Class Constructor
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext>options) : base(options)
+        {
+        }
+
+        // OnConfiguring() method is used to select and configure the data source
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        // Adding Domain Classes as DbSet Properties
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
